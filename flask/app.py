9d6d3a5335b92407bee8e091a939c1c1f1b9483a
Hong Huat <158584424+honghuat-2301911@users.noreply.github.com>
2025-06-18 11:57:19 +0000

    fix o

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello, Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+
