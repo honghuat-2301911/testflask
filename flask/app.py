7b4fc1bdb7ecdba550c15e3cfa1887e42f39cc6b
Hong Huat <158584424+honghuat-2301911@users.noreply.github.com>
2025-06-18 12:43:16 +0000

    qqqqqqqqqqqqqqqqqqqq

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,17 @@
+"""
+This module runs the main Flask application.
+"""
+
 from flask import Flask, render_template
-import mysql.connector
-import os
+# import mysql.connector
+# import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Return a greeting from Flask."""
     return "Hello, Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
-
-
